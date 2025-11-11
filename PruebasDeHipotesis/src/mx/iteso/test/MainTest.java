a92005cc7780aa831cf6e5c5470666ca2a79b53c
guille-alba <guillermo.dealba@iteso.mx>
2025-11-11 13:03:20 -0600

    2° commit

--- PruebasDeHipotesis/src/mx/iteso/test/MainTest.java
+++ PruebasDeHipotesis/src/mx/iteso/test/MainTest.java
@@ -3,7 +3,7 @@ package mx.iteso.test;
 public class MainTest {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
 
